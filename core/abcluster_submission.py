45676fc9b842009c79a8a5ed5c907cb54b1181a3
João Paulo Almeida de Mendonça <70282306+jpalastus@users.noreply.github.com>
2022-10-04 19:34:43 +0200

    Update abcluster_submission.py

--- core/abcluster_submission.py
+++ core/abcluster_submission.py
@@ -6,14 +6,14 @@ import time
 print("\n\n --- AUTOMATION OF ABCLUSTER USAGE --- ")
 print("ATENTION: Some HPC centers have laws against scripts that submit jobs.")
 cont=input("Type 'YES' if you want to continue: ")
-if cont /= "YES":
+if cont != "YES":
 	exit("Leaving...")
 
 os.system("mkdir all_xyz")
 print("Prepare folder for each execution inside 'core'. The folders most contain a adapted copy of job.pbs and abcluster.inp.")
 cont=input("What name you put on the output file (first entry on the ABCLUSTER input):")
 
-#Run it Local
+###Run it Local
 i=1
 for folder in glob.glob("core/*/"):
 	os.system("cd "+folder+"&& ./job.sh ")
@@ -21,11 +21,11 @@ for folder in glob.glob("core/*/"):
 	i=i+1
 
 	
-#Run in cluster
-for folder in glob.glob("core/*/"):
-	os.system("cd "+folder+"&& qsub job.pbs")
-time.sleep(3600)  # tune this value so the cluster waits the computations finish 
-i=1
-for folder in glob.glob("core/*/"):
-	os.system("cd "+folder+cont+"_LM/ && for file in *.xyz; do cp $file ../../../all_xyz/"+str(i)+"$file; done")
-	i=i+1
+####Run in cluster
+#for folder in glob.glob("core/*/"):
+#	os.system("cd "+folder+"&& qsub job.pbs")
+#time.sleep(3600)  # tune this value so the cluster waits the computations finish 
+#i=1
+#for folder in glob.glob("core/*/"):
+#	os.system("cd "+folder+cont+"_LM/ && for file in *.xyz; do cp $file ../../../all_xyz/"+str(i)+"$file; done")
+#	i=i+1
