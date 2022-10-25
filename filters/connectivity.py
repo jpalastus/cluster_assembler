e69127b30e753729defab6f8deaaabe0287439a3
João Paulo Almeida de Mendonça <70282306+jpalastus@users.noreply.github.com>
2022-10-25 21:55:22 +0200

    Update connectivity.py

--- filters/connectivity.py
+++ filters/connectivity.py
@@ -69,65 +69,13 @@ for i in range(len(ncomplexos)):
 
 for i in range(max(ncomplexos)):
   print(cont[i], indexlist[i])
-
-                             
-exit()
-#################################################################################    
-                             
-                             
-                             
-file='HCu8Zn3Cps4Mes3_structures_pbe/HCu8Zn3Cps4Mes3_221.xyz'
-
-mol=read(file)
-cutOff = neighborlist.natural_cutoffs(mol)
-neighborList = neighborlist.NeighborList(cutOff, self_interaction=False, bothways=True)
-neighborList.update(mol)
-ideal_matrix = neighborList.get_connectivity_matrix(sparse=False)
-n_components, component_list = sparse.csgraph.connected_components(ideal_matrix)
-
-print(ideal_matrix)
-
-ncomplexos=[]
-estruturas=[]
-for file in sorted(glob.glob('HCu8Zn3Cps4Mes3_structures_pbe/*.xyz')):
-
-  mol=read(file)
-  cutOff = neighborlist.natural_cutoffs(mol)
-  neighborList = neighborlist.NeighborList(cutOff, self_interaction=False, bothways=True)
-  neighborList.update(mol)
-  matrix = neighborList.get_connectivity_matrix()
-  matrix_nosparse = neighborList.get_connectivity_matrix(sparse=False)
-  n_components, component_list = sparse.csgraph.connected_components(matrix)
+  print("      ---> Saving results in final_representatives...")
+  os.system("mkdir final_representatives")
+  for number in indexlist[i]:
+    os.system("cp "+folder+'/*'+number+'*.xyz final_representatives/.')
     
-  teste_H_lig=True
-  for i in [11]:
-    for j in range(12,len(matrix_nosparse)):
-      teste_H_lig=teste_H_lig and (matrix_nosparse[i][j]==ideal_matrix[i][j])
-  
-  teste_lig_lig=True
-  for i in range(12,len(matrix_nosparse)):
-    for j in range(12,len(matrix_nosparse)):
-      teste_lig_lig=teste_lig_lig and (matrix_nosparse[i][j]==ideal_matrix[i][j])
+    
+    
 
-      
-  print(file,n_components,teste_H_lig,teste_lig_lig)
-  ncomplexos.append(n_components)
-  estruturas.append(file)
-
-from google.colab import drive
-drive.mount('/content/drive')
-
-print("Numero maximo de particoes: ", max(ncomplexos))
-cont=[]
-indexlist=[]
-for i in range(max(ncomplexos)):
-  cont.append(0)
-  indexlist.append([])
-
-for i in range(len(ncomplexos)):
-  cont[(ncomplexos[i]-1)]=cont[ncomplexos[i]-1]+1
-  indexlist[(ncomplexos[i]-1)].append(estruturas[i][-7:-4])
-
-for i in range(max(ncomplexos)):
-  print(cont[i], indexlist[i])
+    
 
